1384483427cfaf61a517df896565bbf9da785bb6
Mohammed <mohammedelnabawy11@gmail.com>
2023-02-25 22:21:58 +0200

    Add user name and password to user data

--- scripts/LocalStorage.js
+++ scripts/LocalStorage.js
@@ -1,3 +1,25 @@
+function generateName() {
+    let length = 7,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    retVal = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;}
+
+userName = generateName();
+
+function generatePassword() {
+    let length = 4,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    retVal = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;}
+
+pass = generatePassword();
+
 function addtolocal(){
     var f_name = document.getElementById("fname").value;
     var l_name = document.getElementById("lname").value;
@@ -8,7 +30,7 @@ function addtolocal(){
     var old = localStorage.getItem("data");
     if(old === null) old = "[]";
 
-    var inputData = {"user1":{"fname":f_name, "lname":l_name, "Address":add, "email":ema, "age":age}};
+    var inputData = {"userName":userName, "fname":f_name, "lname":l_name, "Address":add, "email":ema, "age":age, "password":pass};
     var before = JSON.parse(old);
     before.push(inputData);
     localStorage.setItem("data", JSON.stringify(before));
