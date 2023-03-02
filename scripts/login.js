e19f4d966ce432f7477d7d79f8b338761d683675
Mohammed <mohammedelnabawy11@gmail.com>
2023-03-02 12:36:26 +0200

    add user object to pofile

--- scripts/login.js
+++ scripts/login.js
@@ -11,12 +11,11 @@ function GOlogin(){
     var before = JSON.parse(old);
     for (let i = 0; i < before.length; i++) {
         if (user == before[i].userName && pass == before[i].password) {
-            
+            var objectuserdata = {"userName":before[i].userName, "fname":before[i].fname, "lname":before[i].lname, "Address":before[i].Address, "email":before[i].email, "age":before[i].age, "password":before[i].password};
+            localStorage.setItem("logeduser", JSON.stringify(objectuserdata));
             setTimeout("location.href = 'profile.html';",1000);
             break;
-        }else {
-            alert('ERROR.');
-            break;
-      }
+        }
     }
+    alert("Error in username or password")
 }
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
-    const obj = JSON.parse(localStorage.getItem("data"))[0].user1;
-    const txt = '{"FirstName":"Mohammed", "LastName":"El-Nabawy", "Address":"Cairo","Email":"mohammedelnabawy11@gmail.com", "age":30}';
+    const obj = JSON.parse(localStorage.getItem("logeduser"));
+    // const txt = '{"FirstName":"Mohammed", "LastName":"El-Nabawy", "Address":"Cairo","Email":"mohammedelnabawy11@gmail.com", "age":30}';
+    
     document.getElementById("fname").value = obj.fname;
     document.getElementById("lname").value = obj.lname;
     document.getElementById("Address").value = obj.Address;
